858d8c745f7e64dc6e6ac77b8b24df1fdb78c04c
Rohit Kumar Mahato <mahator99@gmail.com>
2022-11-09 11:41:58 +0530

    started js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const yearLog = doucment.getElementById("yearLog");
+const userForm = doucment.getElementById("userForm");
+const userImg = doucment.getElementById("userImg");
+const userName = doucment.getElementById("userName");
+const userFullName = doucment.getElementById("userFullName");
+const userId = doucment.getElementById("userId");
+const followerBtn = doucment.getElementById("followerBtn");
+const followingBtn = doucment.getElementById("followingBtn");
+const recentBtn = doucment.getElementById("recentBtn");
+const relatedBtn = doucment.getElementById("relatedBtn");
+const profileBox = doucment.getElementById("profileBox");
+const postContainer = doucment.getElementById("postContainer");
+
+
+
 // get user id
 // get user following
 // get related profiles
 // get user followers
 // get public user posts
-
